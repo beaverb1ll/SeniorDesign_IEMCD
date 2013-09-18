773372401dc176f4b599bd2d467b6669dbb6d14f
Mike <tyrlikmike@gmail.com>
2013-09-17 23:34:02 -0400

    Updated Readme

---
+++ main.c
@@ -0,0 +1,32 @@
+// how this will work:
+
+	// parse arguments
+	// open serial port
+	// open barcode (serial or usb?) port
+	// open sql database
+	
+	// loop
+		// wait for barcode
+		// query sql
+		// 	|   |
+		//  |   |
+		//  |    -> If found, send commands to serial 
+		//   ->  Else continue.
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+
+int main(int argc, char const *argv[])
+{
+
+	// loo
+	while (TRUE) 
+	{
+
+	}
+	return 0;
+}
